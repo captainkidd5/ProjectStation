c70692ecf94139f4e544f2e99c88ec1641e7c118
Tucker Higgins <thiggin6@lion.lmu.edu>
2020-07-21 13:41:26 -0700

    added main page and products page

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectStation.Models;
+using ProjectStation.Services;
+
+namespace ProjectStation.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public JsonFileProductService ProductService { get; }
+
+        public ProductsController(JsonFileProductService jsonFileProductService)
+        {
+            this.ProductService = jsonFileProductService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+    }
+}
---
+++ Services/JsonFileProductService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using ProjectStation.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ProjectStation.Services
+{
+    public class JsonFileProductService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; private set; }
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            this.WebHostEnvironment = webHostEnvironment;
+        }
+
+        /// <summary>
+        /// Gets the json file located within the wwwroot. Path library takes care of slashes etc.
+        /// </summary>
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+
+        /// <summary>
+        /// Deserialize Json file into Products.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
