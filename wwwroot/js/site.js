e9d926ed980149c2e0612d8cfe2e1135224914cb
Tucker Higgins <thiggin6@lion.lmu.edu>
2020-07-28 11:08:45 -0700

    font replace

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -29,10 +29,6 @@ $(document).ready(function () {
     });
 });
 
-function scrollToTop() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
 
 
 /* When the user clicks on the button,
