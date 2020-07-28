42c3200966bacacfde294db9375a1707eb9f9654
Tucker Higgins <thiggin6@lion.lmu.edu>
2020-07-28 09:27:34 -0700

    working on scroll to top button

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,6 +4,11 @@
 // Write your Javascript code.
 
 
+function scrollToTop() {
+
+}
+
+
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
