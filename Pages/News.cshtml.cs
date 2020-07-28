dfd4ba471b13fddca53948a1c64d7f268bddc5f6
Tucker Higgins <thiggin6@lion.lmu.edu>
2020-07-28 12:19:17 -0700

    figuring out sections

--- Pages/News.cshtml.cs
+++ Pages/News.cshtml.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ProjectStation.Pages.Shared
+namespace ProjectStation.Pages
 {
-    public class _FooterModel : PageModel
+    public class NewsModel : PageModel
     {
         public void OnGet()
         {
