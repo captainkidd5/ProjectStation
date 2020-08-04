2eeb18fe42cbcf519120f04ebf82ba681726184b
Tucker Higgins <thiggin6@lion.lmu.edu>
2020-08-03 21:57:39 -0700

    save before deleting table

--- Models/Models/Client.cs
+++ Models/Models/Client.cs
@@ -10,16 +10,16 @@ namespace Models
     public class Client
     {
         public int Id { get; set; }
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         [Required(ErrorMessage ="Name is required.")]
         [MinLength(3,ErrorMessage ="Name should contain at least 3 characters.")]
         public string Name { get; set; }
-        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         [Required(ErrorMessage = "Email is required.")]
         [EmailAddress(ErrorMessage ="Invalid email format.")]
         public string Email { get; set; }
         public string PhotoPath { get; set; }
-        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         [Required]
         public ClientType? ClientType { get; set; }
     }
---
+++ Services/Migrations/20200804045338_spGetClientById.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Services.Migrations
+{
+    public partial class spGetClientById : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string procedure = @"Create procedure spGetClientById
+@Id int
+as
+Begin
+	Select * from Clients
+	where Id = @Id
+End";
+
+            migrationBuilder.Sql(procedure);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            string procedure = @"Drop procedure spGetClientById ";
+                          migrationBuilder.Sql(procedure);
+        }
+    }
+}
