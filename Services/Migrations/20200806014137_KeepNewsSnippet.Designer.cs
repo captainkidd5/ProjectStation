cebd0616fcb01a43330f348766620f70b03fa932
Tucker Higgins <thiggin6@lion.lmu.edu>
2020-08-05 21:52:34 -0700

    got news snippets to display properly

--- Services/Migrations/20200806014137_KeepNewsSnippet.Designer.cs
+++ Services/Migrations/20200806014137_KeepNewsSnippet.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Services;
 
 namespace Services.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200806014137_KeepNewsSnippet")]
+    partial class KeepNewsSnippet
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Services/Migrations/20200806014153_spGetNewsSnippetById.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Services.Migrations
+{
+    public partial class spGetNewsSnippetById : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string procedure = @"Create procedure spGetNewsSnippetById
+                                @Id int
+                                as
+                                Begin
+	                                Select * from NewsSnippets
+	                                where Id = @Id
+                                End";
+            migrationBuilder.Sql(procedure);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            string procedure = @"Create procedure spGetNewsSnippetById";
+            migrationBuilder.Sql(procedure);
+        }
+    }
+}
