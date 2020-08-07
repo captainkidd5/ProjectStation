a6a3ebb7f0fd43c2c4f264de1c9b71068ef8da20
Tucker Higgins <thiggin6@lion.lmu.edu>
2020-08-07 14:35:35 -0700

    working on adding currency properly

--- Pages/ShopStuff/Products/AsianSquat.cshtml.cs
+++ Pages/ShopStuff/Products/AsianSquat.cshtml.cs
@@ -1,22 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Models.Models;
+using Services;
 
 namespace ProjectStation.Pages.ShopStuff.Products
 {
     public class AsianSquatModel : PageModel
     {
-        public AsianSquatModel()
-        {
+        public Product Product { get; set; }
+        private readonly IProductRepository productRepository;
+        public string Price { get; set; }
 
+
+        public AsianSquatModel(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+            
         }
 
         public void OnGet()
         {
 
+            this.Product = productRepository.GetProduct(1);
+            this.Price = string.Format(Product.Price.ToString(), "{0:C}", CultureInfo.CreateSpecificCulture("ja-JP"));
         }
     }
 }
---
+++ Services/Migrations/20200807205406_spGetProductById.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Services.Migrations
+{
+    public partial class spGetProductById : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string procedure = @"Create procedure spGetProductById
+                                @Id int
+                                as
+                                Begin
+	                                Select * from Products
+	                                where Id = @Id
+                                End";
+            migrationBuilder.Sql(procedure);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
