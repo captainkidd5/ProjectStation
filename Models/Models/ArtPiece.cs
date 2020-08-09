525fcaa15b8c6adb705b779982d573f9a5b7dd47
Tucker Higgins <thiggin6@lion.lmu.edu>
2020-08-08 17:42:25 -0700

    working on art pieces

---
+++ Models/Models/ArtPiece.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Models.Models
+{
+    public class ArtPiece
+    {
+        public int Id { get; set; }
+        public string Exhibition { get; set; }
+        public string Name { get; set; }
+ 
+        public string Description { get; set; }
+
+        public string PhotoPath { get; set; }
+
+        public string QRPath { get; set; }
+    }
+}
