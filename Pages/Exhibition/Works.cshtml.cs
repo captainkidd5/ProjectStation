107a50ff005ec4feb1c2e674c2403e4ce56a79ae
Tucker Higgins <thiggin6@lion.lmu.edu>
2020-08-08 23:08:19 -0700

    looking good!

--- Pages/Exhibition/Works.cshtml.cs
+++ Pages/Exhibition/Works.cshtml.cs
@@ -4,21 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Models.Models;
 using Services.ArtWork;
 
 namespace ProjectStation.Pages.Exhibition
 {
     public class WorksModel : PageModel
     {
+        public ArtPiece ArtPiece { get; set; }
         private readonly IArtPieceRepository artPieceRepository;
 
         public WorksModel(IArtPieceRepository artPieceRepository)
         {
             this.artPieceRepository = artPieceRepository;
         }
-        public void OnGet()
+        public void OnGet(int id)
         {
-
+            if(id > 0)
+            {
+                this.ArtPiece = artPieceRepository.GetArtPiece(id);
+            }
         }
     }
 }
