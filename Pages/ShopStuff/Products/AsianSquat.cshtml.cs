7b24be6635d10614cc358489dd9683ca08c2dca8
Tucker Higgins <thiggin6@lion.lmu.edu>
2020-08-12 16:34:29 -0700

    session cart working

--- Pages/ShopStuff/Products/AsianSquat.cshtml.cs
+++ Pages/ShopStuff/Products/AsianSquat.cshtml.cs
@@ -2,26 +2,42 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Models;
 using Models.Models;
+using ProjectStation.Components;
 using Services;
+using Services.Shopping;
 
 namespace ProjectStation.Pages.ShopStuff.Products
 {
     public class AsianSquatModel : PageModel
     {
         public Product Product { get; set; }
+
+        [BindProperty]
+        public int Quantity { get; set; }
+
+        private readonly IShoppingCartRepository cartRepository;
+        private readonly SignInManager<IdentityUser> signInManager;
         private readonly IProductRepository productRepository;
-        public string Price { get; set; }
+        public string Price { get; 
+            set; }
 
         public List<string> ImagePaths { get; set; }
 
-        public AsianSquatModel(IProductRepository productRepository)
+        public AsianSquatModel(IShoppingCartRepository cartRepository, SignInManager<IdentityUser> signInManager, IProductRepository productRepository)
         {
+            this.cartRepository = cartRepository;
+            this.signInManager = signInManager;
             this.productRepository = productRepository;
-            
+
         }
 
         public void OnGet()
@@ -36,6 +52,64 @@ namespace ProjectStation.Pages.ShopStuff.Products
             };
             this.Product = productRepository.GetProduct(1);
             this.Price = string.Format(Product.Price.ToString("C", CultureInfo.CreateSpecificCulture("ja-JP")));
+        }
+        public ShoppingCart ShoppingCart { get; set; }
+        public void OnPost()
+        {
+            this.Product = productRepository.GetProduct(1);
+            if (signInManager.IsSignedIn(User))
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier).ToString();
+                ShoppingCart = cartRepository.GetCart(userId);
+                if (ShoppingCart == null)
+                {
+                    string cartID = Guid.NewGuid().ToString();
+                    ShoppingCart = cartRepository.CreateCart(userId, cartID);
+                }
+
+                cartRepository.AddItem(Product.ID, Quantity);
+            }
+            else if (HttpContext.Session.Get("ShoppingCart") != null)
+            {
+                AddToSessionCart();
+            }
+            else
+            {
+                ShoppingCart = new ShoppingCart()
+                { CartId = Guid.NewGuid().ToString(), DateCreated = DateTime.Now, UserId = null };
+                HttpContext.Session.SetObjectAsJson("ShoppingCart", ShoppingCart);
+                List<CartItem> cartItems = new List<CartItem>();
+                HttpContext.Session.SetObjectAsJson("CartItems", cartItems);
+                this.ShoppingCart = ShoppingCart;
+
+                AddToSessionCart();
+            }
+        }
+
+        private void AddToSessionCart()
+        {
+            List<CartItem> cartItems = new List<CartItem>();
+            ShoppingCart = HttpContext.Session.GetObjectFromJson<ShoppingCart>("ShoppingCart");
+            cartItems = HttpContext.Session.GetObjectFromJson<List<CartItem>>("CartItems");
+
+            CartItem cartItem = cartItems.Find(x => x.ProductId == this.Product.ID);
+            if (cartItem != null)
+            {
+                cartItem.Quantity += Quantity;
+            }
+            else
+            {
+                cartItems.Add(new CartItem()
+                {
+                    CartId = ShoppingCart.CartId,
+                    DateCreated = DateTime.Now,
+                    Id = this.Product.ID,
+                    ProductId = this.Product.ID,
+                    Quantity = this.Quantity
+                });
+            }
+            
+            HttpContext.Session.SetObjectAsJson("CartItems", cartItems);
         }
     }
 }
