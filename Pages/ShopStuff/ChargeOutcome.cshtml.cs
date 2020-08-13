f2a02715d38e36cfe2e290c3789820c2bc39161e
Tucker Higgins <thiggin6@lion.lmu.edu>
2020-08-13 15:20:14 -0700

    quicksave

---
+++ Pages/ShopStuff/ChargeOutcome.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjectStation.Pages.ShopStuff
+{
+    public enum ChargeOutCome
+    {
+        Failed = 0,
+        Succeeded = 1
+    }
+
+    public class ChargeOutcomeModel : PageModel
+    {
+        public string ChargeOutcomeMessage { get; set; }
+
+        public void OnGet(int chargeOutCome)
+        {
+            if ((ChargeOutCome)chargeOutCome == ChargeOutCome.Failed)
+            {
+                ChargeOutcomeMessage = "Payment failed, please try again.";
+            }
+            else if ((ChargeOutCome)chargeOutCome == ChargeOutCome.Succeeded)
+            {
+                ChargeOutcomeMessage = "Your payment has been received and your order is being processed!";
+            }
+            else
+            {
+                throw new Exception("Not a valid enum value for chargeoutcome");
+            }
+        }
+    }
+}
--- Pages/ShopStuff/Checkout.cshtml.cs
+++ Pages/ShopStuff/Checkout.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -14,6 +15,8 @@ namespace ProjectStation.Pages.ShopStuff
     {
         //[BindProperty]
         //public Customer Customer { get; set; }
+
+        private readonly string WebHookSecret = "webhookSecret";
 
         public CheckoutModel()
         {
@@ -39,6 +42,38 @@ namespace ProjectStation.Pages.ShopStuff
 
             }) ;
 
+            return Page();
+        }
+
+        public IActionResult ChargeChange()
+        {
+            var json = new StreamReader(HttpContext.Request.Body).ReadToEnd();
+
+            try
+            {
+                var stripeEvent = EventUtility.ConstructEvent(json,
+                    Request.Headers["Stripe-Signature"], WebHookSecret, throwOnApiVersionMismatch: true);
+                Charge charge = (Charge)stripeEvent.Data.Object;
+                switch (charge.Status)
+                {
+                    case "succeeded":
+                        //This is an example of what to do after a charge is successful
+                        charge.Metadata.TryGetValue("Product", out string Product);
+                        charge.Metadata.TryGetValue("Quantity", out string Quantity);
+                        //   Database.ReduceStock(Product, Quantity);
+                        RedirectToPage("/shopstuff/ChargeOutcome", new { id = 1 });
+                        break;
+                    case "failed":
+                        //Code to execute on a failed charge
+                        RedirectToPage("/shopstuff/ChargeOutcome", new { id = 0 });
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+              //  e.Ship(HttpContext);
+                return BadRequest();
+            }
             return Page();
         }
 
