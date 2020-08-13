cfc97855b9b7335a42b6ee5154de64ccefe746a2
Tucker Higgins <thiggin6@lion.lmu.edu>
2020-08-13 14:16:53 -0700

    stripe test worked

--- Pages/ShopStuff/Checkout.cshtml.cs
+++ Pages/ShopStuff/Checkout.cshtml.cs
@@ -4,23 +4,47 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Razor;
 using Models.Models.ShoppingStuff;
+using Stripe;
 
 namespace ProjectStation.Pages.ShopStuff
 {
     public class CheckoutModel : PageModel
     {
-        [BindProperty]
-        public Customer Customer { get; set; }
+        //[BindProperty]
+        //public Customer Customer { get; set; }
 
         public CheckoutModel()
         {
 
         }
 
+        public IActionResult OnPostCharge(string stripeEmail, string stripeToken)
+        {
+            var customerService = new CustomerService();
+            var chargeService = new ChargeService();
+            var customer = customerService.Create(new CustomerCreateOptions()
+            {
+                Email = stripeEmail,
+                Source = stripeToken,
+            });
+
+            var charge = chargeService.Create(new ChargeCreateOptions()
+            {
+                Amount = 500,
+                Description = "test",
+                Currency = "usd",
+                Customer = customer.Id
+
+            }) ;
+
+            return Page();
+        }
+
         public void OnGet()
         {
-            this.Customer = new Customer();
+           // this.Customer = new Customer();
         }
     }
 }
