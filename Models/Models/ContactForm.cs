462ddf94115b7ed4e6d302fbf8fa0d0e27873c76
Tucker Higgins <thiggin6@lion.lmu.edu>
2020-08-16 21:09:17 -0700

    working on privacy policy

---
+++ Models/Models/ContactForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Models.Models
+{
+    public class ContactForm
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        [MinLength(3, ErrorMessage = "Name should contain at least 3 characters.")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Name (reading)")]
+        public string NameReading { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
+        [Phone(ErrorMessage ="Invalid phone number")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        [Required]
+        public string CompanyName { get; set; }
+
+        [Required]
+        [MaxLength(500, ErrorMessage = "Max message length is 500 characters.")]
+        public string Inquiry { get; set; }
+
+        [Display(Name = "Privacy Policy")]
+        [Range(typeof(bool), "true", "true", ErrorMessage = "You must review the privacy policy to submit an inquiry.")]
+        public bool AgreePolicy { get; set; }
+    }
+}
--- Pages/Contact.cshtml.cs
+++ Pages/Contact.cshtml.cs
@@ -2,42 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Models.Models;
 
 namespace ProjectStation.Pages
 {
     public class ContactModel : PageModel
     {
+        [BindProperty]
+        public ContactForm ContactForm{ get; set; }
 
         [BindProperty]
         public bool EmailNotify { get; set; }
 
-        [BindProperty]
-        public string Name { get; set; }
+        public IEmailSender EmailSender { get; }
 
-        [BindProperty]
-        public string Email { get; set; }
-
-        public string SubmitMessage { get; set; }
-
-
-
-
-        public ContactModel( )
+        public ContactModel(IEmailSender emailSender)
         {
-
+            this.EmailSender = emailSender;
         }
 
         public void OnGet()
         {
-
+            ContactForm = new ContactForm();
         }
 
-        public void OnPost(int id)
+        public void OnPost()
         {
+            if(ModelState.IsValid)
+            {
 
+            }
         }
     }
 }
